023d94fceb1bfe9b3003029f772fe3ca8233a86c
Fatai <116838944+FataiAzeez@users.noreply.github.com>
2023-01-09 16:11:03 +0000

    Updating the index file with the app content

--- index.py
+++ index.py
@@ -1,10 +1,20 @@
 # import neccessary libraries, modules and packages
 from dash import html, dcc, Input, Output
-from app import app
+# from app import app
 from pages import covid, search, about, login
 
-# setting the server
+import dash
+
+# Create a Dash app with Bootstrap styling
+app = dash.Dash(__name__, use_pages=True, external_stylesheets=[
+    'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css'],
+      meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+    ])
+
 server = app.server
+app.config.suppress_callback_exceptions = True
+
 
 
 # embedding the layout 
